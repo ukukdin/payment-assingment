fbe277f985513de217082272d790dce7a235aade
엄인국 <eum714211@gmail.com>
2025-12-29 22:52:01 +0900

    infrastructure 서브프로젝트에 `kotlin-jpa` 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,6 +91,7 @@ project(":modules:infrastructure") {
         apply(plugin = "org.springframework.boot")
         apply(plugin = "io.spring.dependency-management")
         apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+        apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
     }
 }
 
